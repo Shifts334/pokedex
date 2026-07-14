b16ac086b5b806cb9a8d735bbff1f2890da6eb45
Edouard <ea.ybanez334@gmail.com>
2026-07-15 00:50:16 +0800

    chore: fleshed out functions for  pokemon.ts

--- pokedex/lib/constants.ts
+++ pokedex/lib/constants.ts
@@ -1,4 +1,4 @@
-// constraints should be 1010 , i'm basing completely off on the specs paper they gave
+// constraints stick with 1010 , i'm basing completely off on the specs paper they gave
 
-//export const MAX_POKEMON_ID = 1010;
+// export const MAX_POKEMON_ID = 1010;
 
---
+++ pokedex/lib/pokeapi.ts
@@ -0,0 +1,31 @@
+import { PokemonDTO, PokemonSpeciesDTO, Pokemon } from "@/types/pokemon";
+import { describe } from "node:test";
+
+function toPokemon (
+    dto:PokemonDTO,
+    species: PokemonSpeciesDTO,
+    weaknesses: string[]
+): Pokemon {
+    return{
+        id: dto.id,
+        name: dto.name,
+        heightM: dto.height / 10,
+        weightKg: dto.weight / 10,
+        types: dto.types.map(t=>t.type.name),
+        category:
+            species.genera.find(g=>g.language.name === "en")?.genus ?? "Unknown",  
+        description:
+            species.flavor_text_entries.find(f=>f.language.name === "en")?.flavor_text.replace
+            (/[\n\f\r]/g, " ").trim() ?? "", 
+        stats: dto.stats.map(s=> ({name: s.stat.name, value: s.base_stat})),
+        abilities: dto.abilities.map(a=> a.ability.name),
+        artworkUrl: artworkUrl(dto.id), 
+        spriteFallback:
+            dto.sprites.other["official-artwork"].front_default ??
+            dto.sprites.front_default,
+        weaknesses,
+        
+    }
+}
+
+
